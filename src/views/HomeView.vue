12d0de7dd1decab7b50ad0a7babafa75f1ac4d18
AnthonyLoche <anthonylocheifc@gmail.com>
2024-07-02 20:07:48 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import axios from 'axios';
+
+async function loginWithGitHub() {
+  console.log('Iniciando login com GitHub...')
+  try {
+    const response = await axios.get('https://git-node.vercel.app/auth/github/');
+  
+
+    console.log(response.data);
+
+  }catch(error) {
+    console.error('Erro ao iniciar o login com GitHub:', error);
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <button @click="loginWithGitHub">Login com GitHub</button>
+  </div>
+</template>
