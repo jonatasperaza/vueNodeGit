0b4603f8880df55c0428c16443a2957df1d67781
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-07-02 20:48:44 -0300

    feat: add GitHub login functionality and display user information
    This commit adds the functionality to log in with GitHub by clicking the "Login with GitHub" button. It also fetches the user information from the API and displays the user's login and avatar. This improves the user experience by allowing users to authenticate with their GitHub accounts and see their information on the home page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,41 @@
-<script setup>
-import axios from 'axios';
-
-async function loginWithGitHub() {
-  console.log('Iniciando login com GitHub...')
-  try {
-    const response = await axios.get('https://git-node.vercel.app/auth/github/');
-  
-
-    console.log(response.data);
-
-  }catch(error) {
-    console.error('Erro ao iniciar o login com GitHub:', error);
-  }
-}
-
-</script>
-
 <template>
-  <div>
-    <button @click="loginWithGitHub">Login com GitHub</button>
+  <div id="app">
+    <button @click="loginWithGitHub">Login with GitHub</button>
+    <div v-if="user">
+      <h2>Welcome, {{ user.login }}</h2>
+      <img :src="user.avatar_url" alt="User Avatar" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const user = ref(null);
+
+const loginWithGitHub = () => {
+  window.location.href = 'https://git-node.vercel.app/auth/github';
+};
+
+const fetchUser = async () => {
+  try {
+    const response = await axios.get('https://git-node.vercel.app/api/user', { withCredentials: true });
+    user.value = response.data.user;
+  } catch (error) {
+    console.error('User not authenticated', error);
+  }
+};
+
+onMounted(() => {
+  fetchUser();
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
